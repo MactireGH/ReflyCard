3493b2f2aad415dafd98f30e85fec589946ac09c
Nikita Zolotarev <148555893+MactireGH@users.noreply.github.com>
2025-10-29 12:01:32 +0300

    Creating a menu for working with a post

--- bot/keyboards/common.py
+++ bot/keyboards/common.py
@@ -1,11 +1,31 @@
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import (ReplyKeyboardMarkup,
+                           InlineKeyboardMarkup,
+                           KeyboardButton,
+                           InlineKeyboardButton)
 
 
 MAIN_BTN_CREATE = "Создать публикацию"
 MAIN_BTN_SCHEDULER = "Планировщик"
 
-def main_menu_keyboard():
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton(MAIN_BTN_CREATE), KeyboardButton(MAIN_BTN_SCHEDULER))
+# Callback for inline
+ACT_PUBLISH_NOW = 'act:publish_now'
+ACT_SCHEDULE = 'act:schedule'
+ACT_EDIT = 'act:edit'
+ACT_CANCEL = 'act:cancel'
 
-    return keyboard
+def main_menu_keyboard():
+    keyboard_reply = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard_reply.add(KeyboardButton(MAIN_BTN_CREATE), KeyboardButton(MAIN_BTN_SCHEDULER))
+
+    return keyboard_reply
+
+def post_action_keyboard() -> InlineKeyboardMarkup:
+    """Функция для создания инлайн-клавиатуры с предпросмотром поста."""
+
+    keyboard_inline = InlineKeyboardMarkup()
+    keyboard_inline.add(InlineKeyboardButton(text="Опубликовать сейчас", callback_data=ACT_PUBLISH_NOW))
+    keyboard_inline.add(InlineKeyboardButton(text="Запланировать", callback_data=ACT_SCHEDULE))
+    keyboard_inline.add(InlineKeyboardButton(text="Редактировать", callback_data=ACT_EDIT))
+    keyboard_inline.add(InlineKeyboardButton(text="Отмена", callback_data=ACT_CANCEL))
+
+    return keyboard_inline
