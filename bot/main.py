c073f57b784afd0e38a6c5afdcf4566167332da8
Nikita Zolotarev <148555893+MactireGH@users.noreply.github.com>
2025-10-29 10:57:04 +0300

    We've changed the approach to handling menu buttons.

--- bot/main.py
+++ bot/main.py
@@ -1,29 +1,36 @@
 from telebot import TeleBot
+from telebot.types import Message
 
-from core.config import BOT_TOKEN
-from core.messages_bot import FIRST_RUN_APP, CREATE_PUBLIC, SCHEDULER_PUBLIC
-from bot.keyboards import keyboard_main, BTN_CREATE, BTN_SCHEDULE
-
+from settings.config import get_bot_token
+from settings import messages
+from bot.keyboards.common import (
+    main_menu_keyboard,
+    MAIN_BTN_CREATE,
+    MAIN_BTN_SCHEDULER,
+)
 
 def app_run():
-    if not  BOT_TOKEN:
-        raise RuntimeError('BOT_TOKEN не найден. Создай .env и вставь токен.')
-
-    bot = TeleBot(BOT_TOKEN, parse_mode='HTML')
+    bot = TeleBot(get_bot_token(), parse_mode='HTML')
 
     @bot.message_handler(commands=["start"])
-    def handle_start(m):
-        bot.send_message(m.chat.id, FIRST_RUN_APP, reply_markup=keyboard_main())
+    def handle_start(message: Message):
+        bot.delete_state(message.from_user.id, message.chat.id)
+        bot.send_message(message.chat.id, messages.FIRST_RUN_APP, reply_markup=main_menu_keyboard())
 
-    @bot.message_handler(func=lambda m: m.text == BTN_CREATE)
-    def handle_create(m):
-        bot.send_message(m.chat.id, CREATE_PUBLIC)
+    @bot.message_handler(func= lambda x: x.text in {MAIN_BTN_CREATE, MAIN_BTN_SCHEDULER})
+    def handler_main_menu(message: Message):
+        if message.text == MAIN_BTN_CREATE:
+            return flow_create_publication(message)
+        if message.text == MAIN_BTN_SCHEDULER:
+            return flow_scheduler(message)
+        return None
 
-    @bot.message_handler(func=lambda m: m.text == BTN_SCHEDULE)
-    def handle_schedule(m):
-        bot.send_message(m.chat.id, SCHEDULER_PUBLIC)
+    def flow_create_publication(message: Message):
+        bot.send_message(message.chat.id, messages.CREATE_PUBLIC)
 
-    print("Bot is running…")
+    def flow_scheduler(message: Message):
+        bot.send_message(message.chat.id, messages.SCHEDULER_PUBLIC)
+
     bot.infinity_polling(skip_pending=True)
 
 if __name__ == "__main__":
