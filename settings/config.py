5b25e204b79d0bb093e8555b87110c00ca77ed66
Nikita Zolotarev <148555893+MactireGH@users.noreply.github.com>
2025-11-08 02:10:10 +0300

    Added a file config.py and created function for token.

---
+++ settings/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+def get_bot_token() -> str:
+    """Функция для проверки наличия и передачи токена."""
+
+    token = os.getenv("BOT_TOKEN")
+
+    if not token:
+        raise RuntimeError('Token not found')
+
+    return token
