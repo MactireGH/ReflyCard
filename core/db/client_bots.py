753c3f7d1bfcfac3c0a0b8bb7e7db9487ea4d5e6
Nikita Zolotarev <148555893+MactireGH@users.noreply.github.com>
2025-11-28 11:09:59 +0300

    Create client_bots.py

---
+++ core/db/client_bots.py
@@ -0,0 +1,39 @@
+import sqlite3
+from contextlib import closing
+from typing import cast
+from core.db.create_database import ClientBot
+from core.db.create_database import get_connection, row_to_class
+
+
+def get_client_token(user_id: int) -> ClientBot | None:
+    """Searches for and returns the client bot if found in the database"""
+
+    with closing(get_connection()) as connection, closing(connection.cursor) as cursor:
+        connection, cursor = cast(sqlite3.Connection, connection), cast(sqlite3.Cursor, cursor)
+
+        cursor.execute("""SELECT bot_id, user_id, bot_name, bot_username, bot_token, bot_created
+                          FROM client_bots
+                          WHERE user_id = ?""", (user_id,))
+
+        bot = cursor.fetchone()
+        return row_to_class(ClientBot, bot) if bot else None
+
+
+def create_client_bot(user_id: int, bot_name: str, bot_username: str, bot_token: str, bot_created: str) -> ClientBot:
+    """Creates and saves new client bot to the database."""
+
+    with closing(get_connection()) as connection, closing(connection.cursor()) as cursor:
+        connection, cursor = cast(sqlite3.Connection, connection), cast(sqlite3.Cursor, cursor)
+
+        cursor.execute("""INSERT INTO client_bots (user_id, bot_name, bot_username, bot_token, bot_created)
+                          VALUES (?, ?, ?, ?, ?)""", (user_id, bot_name, bot_username, bot_token, bot_created))
+
+        connection.commit()
+        bot_id = cursor.lastrowid
+
+        return ClientBot(bot_id=bot_id,
+                         user_id=user_id,
+                         bot_name=bot_name,
+                         bot_username=bot_username,
+                         bot_token=bot_token,
+                         bot_created=bot_created)
